29ed77ca6c17126bafc71d5874792f222a8b09bf
Nate Aquino <naquino14@outlook.com>
2025-01-06 22:32:12 -0500

    Blinky test

--- app/src/main.c
+++ app/src/main.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/linker/linker-defs.h>
 
+// get led node from dt
+#define LED0_NODE DT_ALIAS(led0)
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
 int main(void) {
-    while (1) {
-        printf("Hello World! %s\n", CONFIG_BOARD);
-        printf("Address of sample %p\n", (void *)__rom_region_start);
-        fflush(stdout);
+    static bool state = true;
+
+    // configure gpio
+    if (!gpio_is_ready_dt(&led)) {
+        printk("LED is not ready, dying...\n");
+        return 0;
+    }
+
+    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) {
+        printk("Cant configure LED, dying...\n");
+        return 0;
+    }
+
+    for (;;) {
+        int ret = gpio_pin_toggle_dt(&led);
+        int read_state = gpio_pin_get_dt(&led);
+        printk("Hello World! %s\tState: %s\tRead State: %s\t", CONFIG_BOARD, state ? "ON " : "OFF", read_state == GPIO_OUTPUT_HIGH ? "HI" : "LO");
+        printk("%s%d\n\n", ret == 0 ? "    OK: " : "NOT OK: ", ret);
+
+        state = !state;
+
         k_msleep(500);
     }
     return 0;
