8fd9b40c90b08d7d492b38945e4f3751ec0ae21e
Nate Aquino <naquino14isaboss@gmail.com>
2025-07-13 21:14:52 -0400

    Gps test code

---
+++ app/src/gps-uart.h
@@ -0,0 +1,6 @@
+#ifndef GPS_UART_H
+#define GPS_UART_H
+
+void begin_gps_uart();
+
+#endif // GPS_UART_H
--- app/src/main.c
+++ app/src/main.c
@@ -4,12 +4,18 @@
 
 #include "built-in-test.h"
 #include "roles.h"
+#include "gps-uart.h"
 
 int main(void) {
     bool configured = role_config();
     if (!configured)
         return 1;
 
+        
+    #if defined(CONFIG_DEVICE_ROLE) && (CONFIG_DEVICE_ROLE == DEF_ROLE_TRC)
+    begin_gps_uart();
+    #endif
+        
     run_bit();
 
     return 0;
