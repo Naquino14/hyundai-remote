3ab153687156532a7e29439c123db464b6fa45a3
Nate Aquino <naquino14isaboss@gmail.com>
2025-07-13 21:16:56 -0400

    Remove gps test

--- app/src/gps-uart.h
+++
@@ -1,6 +0,0 @@
-#ifndef GPS_UART_H
-#define GPS_UART_H
-
-void begin_gps_uart();
-
-#endif // GPS_UART_H
--- app/src/main.c
+++ app/src/main.c
@@ -4,18 +4,12 @@
 
 #include "built-in-test.h"
 #include "roles.h"
-#include "gps-uart.h"
 
 int main(void) {
     bool configured = role_config();
     if (!configured)
         return 1;
-
-        
-    #if defined(CONFIG_DEVICE_ROLE) && (CONFIG_DEVICE_ROLE == DEF_ROLE_TRC)
-    begin_gps_uart();
-    #endif
-        
+    
     run_bit();
 
     return 0;
